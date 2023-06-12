bc8c9a37ce2df30a932a0f1f0e18d5c604851052
AntonellaLedesma2 <136131444+AntonellaLedesma2@users.noreply.github.com>
2023-06-12 13:22:05 -0300

    Add files via upload

---
+++ Multiplo de 17.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool EsMultDe17(int n);
+int main(){
+	string msg="no es multiplo";
+	int num;
+	cout<<"ingrese un numero"<<endl;
+	cin>>num;
+	if(EsMultDe17(num)){
+		msg="es multiplo";
+	}
+	
+	cout<<msg<<endl;
+	return 0 ;
+	
+}
+bool EsMultDe17(int n){
+	bool flag=false;
+	if(n %17 ==0)
+	{
+		flag =true;
+	}
+		return flag;
+	
+}
